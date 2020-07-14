a9532ed4d2c79b0dc0945b79358b9e20e7966f9b
Cooper Sanders <tromboneguy@coopersanders.com>
2020-07-14 18:43:26 -0400

    Deleted all buildings except Watt, and all architectures except LSTM. I need to just focus on getting these done for the first release. The other features may return in a later release.

--- power_forecasting/architectures/__init__.py
+++ power_forecasting/architectures/__init__.py
@@ -3,6 +3,4 @@ Export all architecture objects supported by the API
 """
 
 from power_forecasting.architectures._architecture import Architecture
-from power_forecasting.architectures._prophet import Prophet
 from power_forecasting.architectures._lstm import LSTM
-from power_forecasting.architectures._arimax import ARIMAX
