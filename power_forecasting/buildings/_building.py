51117ce9c430a6c574923e036bffe7e73ccac816
Cooper Sanders <tromboneguy@coopersanders.com>
2020-07-14 10:57:08 -0400

    designed and documented base Building class

--- power_forecasting/buildings/_building.py
+++ power_forecasting/buildings/_building.py
@@ -1,2 +1,19 @@
+"""
+Contains the Building base class. This locates the model files.
+"""
+
+# TODO: find a way to input this path string
+PATH: str = "/home/cooper/research/models"
+
 class Building:
-    pass
+    """
+    Base class for all buildings, that gives the location of the saved models.
+    """
+    def __init__(self, building_name: str):
+        """
+        Initiate a building object storing its model location
+        :param building_name: specific building name;
+            capitalization insensitive
+        """
+        self.name: str = building_name
+        self.path: str = PATH + building_name.lower()
