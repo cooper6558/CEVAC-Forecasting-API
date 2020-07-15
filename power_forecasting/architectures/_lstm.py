2858f2773e4eafd7aa06430028fb167fad1362a3
Cooper Sanders <tromboneguy@coopersanders.com>
2020-07-15 10:56:49 -0400

    added impute function

--- power_forecasting/architectures/_lstm.py
+++ power_forecasting/architectures/_lstm.py
@@ -4,11 +4,14 @@ LSTM Architecture file
 
 from power_forecasting.architectures._architecture import Architecture
 import tensorflow as tf
+from tensorflow.keras.models import Sequential
 import pickle
 import pandas as pd
 import os
 from sklearn.utils.testing import ignore_warnings
 from sklearn.exceptions import ConvergenceWarning
+from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import KNeighborsRegressor
 from sklearn.experimental import enable_iterative_imputer
 from sklearn.impute import IterativeImputer
 
@@ -33,26 +36,56 @@ class LSTM(Architecture):
         super().__init__(building_name, building_path)
 
         with open(f"{building_path}/LSTM/scale.pkl", "rb") as file:
-            self.scale = pickle.load(file)
+            self.scale: tuple = pickle.load(file)
 
-        self.model = tf.keras.models.load_model(f"{building_path}/LSTM/model")
+        self.model: Sequential = tf.keras.models.load_model(
+            f"{building_path}/LSTM/model"
+        )
 
     # TODO: move this decorator to the impute function
     @ignore_warnings(category=ConvergenceWarning)
     def predict(
             self,
-            start_date: pd.Timestamp,
-            future_range: pd.Timedelta,
             historical_data: pd.DataFrame,
             future_data: pd.DataFrame,
             verbose: bool
-    ):
+    ) -> pd.Series:
         """
         Produce a dynamic forecast by recursively generating step forecasts.
-        :param start_date: start of forecast
-        :param future_range: range of future (predicted) data required
         :param historical_data: historical data for forecast
         :param future_data: future (predicted) data for forecast
         :param verbose: whether to print forecasting progress
         :return: time series of power consumption forecast
         """
+
+
+def impute(data: pd.DataFrame) -> pd.DataFrame:
+    """
+    Impute missing data.
+    :param data: data frame with missing values
+    :return: the same data frame, with missing values filled in
+    """
+    scaler: StandardScaler = StandardScaler()
+    base_date: pd.Timestamp = data.index[0]
+
+    data["Days"] = data.index.map(
+        lambda date: (date - base_date).days
+    )
+
+    # TODO: would this work without iloc?
+    data.iloc[:, :] = scaler.fit_transform(data)
+
+    imputer: IterativeImputer = IterativeImputer(
+        estimator=KNeighborsRegressor(n_neighbors=30),
+        max_iter=10,
+        verbose=0
+    )
+
+    imputed_data: pd.DataFrame = pd.DataFrame(
+        columns=list(data.columns),
+        index=data.index
+    )
+
+    imputed_data.iloc[:, :] = imputer.fit_transform(data.iloc[:, :])
+    imputed_data.iloc[:, :] = scaler.inverse_transform(imputed_data.iloc[:, :])
+    return imputed_data.drop(columns=["Days"])
