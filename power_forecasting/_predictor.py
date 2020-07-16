e3d5c33b45769226281dee489131a92a172ebca4
Cooper Sanders <tromboneguy@coopersanders.com>
2020-07-16 17:37:02 -0400

    Completely redid the inner structure behind loading and passing around data. It is now loaded by the forecast method, and passed to the architecture as an argument.

--- power_forecasting/_predictor.py
+++ power_forecasting/_predictor.py
@@ -5,9 +5,16 @@ provides a forecast function.
 """
 
 import pandas as pd
+import numpy as np
 from typing import Type
 from power_forecasting.architectures import Architecture
 from power_forecasting.buildings import Building
+# TODO: put group_by_hour in _predictor.py
+from power_forecasting._load_data import (
+    load_historical_data,
+    load_future_data,
+    group_by_hour
+)
 
 
 class Predictor:
@@ -25,18 +32,18 @@ class Predictor:
         :param architecture: desired model architecture
         :param building: desired building for which to produce a forecast
         """
-        self.building: Building = building()
-        self.architecture: Architecture = architecture(
-            building_path=self.building.path,
-            building_name=self.building.name
+        self._building: Building = building()
+        self._architecture: Architecture = architecture(
+            building_name=self._building.name
         )
 
     # TODO: argument = symbols are not separated by spaces
+    # args: start_date, future_range, history_range, future_data, verbose
     def forecast(
             self,
             start_date: pd.Timestamp = None,
             future_range: pd.Timedelta = None,
-            historical_data: pd.DataFrame = None,
+            history_range: pd.Timedelta = None,
             future_data: pd.DataFrame = None,
             verbose: bool = True
     ) -> pd.Series:
@@ -44,35 +51,63 @@ class Predictor:
         Forecast power consumption.
         :param start_date: start of forecast
         :param future_range: range of future (predicted) data required
-        :param historical_data: historical data for forecast
-            (overrides data fetched by Predictor() initialization)
+        :param history_range: range of previous data to use
         :param future_data: future (predicted) data for forecast
             (overrides data fetched by Predictor() initialization)
         :param verbose: whether to print forecasting progress
         :return: time series of power consumption forecast
         """
-        # set defaults
-        start_date = (
-            self.architecture.date
-            if start_date is None else start_date
+        data: pd.DataFrame = load_historical_data(
+            self._building
         )
-        future_range = (
-            pd.to_timedelta(1, unit="D")
-            if future_range is None else future_range
-        )
-        historical_data = (
-            self.architecture.data[self.architecture.data.index <= start_date]
-            if historical_data is None else historical_data
-        )
-        future_data = (
-            self.architecture.data[
-                start_date < self.architecture.data.index
-                <= start_date + future_range
-            ].drop(columns=["Power [kW]"])
-            if future_data is None else future_data
+        if start_date is None:
+            start_date = data.index[-1] + pd.Timedelta(hours=1)
+
+        if future_range is None:
+            future_range = pd.Timedelta(days=1)
+
+        if history_range is None:
+            history_range = pd.Timedelta(weeks=2)
+
+        if future_data is None:
+            future_data = data.copy()
+        else:
+            # TODO: is there a more "Pythonic" way to do this?
+            for column in future_data:
+                future_data[column] = group_by_hour(future_data[column])
+        future_data = future_data.reindex(pd.date_range(
+            start_date,
+            start_date + future_range - pd.Timedelta(hours=1),
+            freq="h",
+            name=future_data.index.name
+        ))
+
+        future_data.fillna(
+            load_future_data(self._building, future_range),
+            inplace=True
         )
 
-        return self.architecture.predict(
+        historical_data: pd.DataFrame = data.copy()
+        historical_data = historical_data.reindex(pd.date_range(
+            start_date - history_range,
+            start_date - pd.Timedelta(hours=1),
+            freq="h",
+            name=historical_data.index.name
+        ))
+
+        # TODO: there has to be a better way to do this
+        for dataframe in (historical_data, future_data):
+            for column in dataframe:
+                if dataframe[column].isna().all():
+                    raise IndexError("No column may be entirely NaN")
+
+        # TODO: this should just be a testing feature
+        future_data = future_data.mask(
+            np.random.random(future_data.shape) < .2
+        )
+        print(historical_data)
+        print(future_data)
+        return self._architecture.predict(
             historical_data,
             future_data,
             verbose
--- power_forecasting/architectures/_architecture.py
+++ power_forecasting/architectures/_architecture.py
@@ -5,7 +5,7 @@ data.
 """
 
 import pandas as pd
-from CEVAC.Connectors import SQLConnector
+import os
 
 
 class Architecture:
@@ -13,180 +13,18 @@ class Architecture:
     Base class for all architectures. Mainly serves to load data.
     """
 
-    def __init__(self, building_name: str, building_path: str) -> None:
+    def __init__(self, building_name: str) -> None:
         """
         Initiate an architecture object storing data required for forecasting
         :param building_name: specific building name;
             capitalization insensitive
-        :param building_path: path to the building's directory
         """
-        self.data: pd.DataFrame
-        self.date: pd.Timestamp
-        self.data, self.date = load_all_data(building_name)
-        self.path: str = building_path
+        self.path: str = os.getenv("FORECAST_MODEL_DIR") + "/" + building_name
 
+    # TODO: fix these args
     def predict(self, *args) -> pd.Series:
         """
         Prototype for predict function. This is never used; only overridden.
         :return: empty time series
         """
-        self.date = args
         return pd.Series()
-
-
-def load_all_data(
-        building_name: str,
-        future_range: pd.Timedelta = None
-) -> (pd.DataFrame, pd.Timestamp):
-    """
-    Read from SQL Server, and compile everything into one dataframe.
-    :param building_name: specific building name;
-        capitalization insensitive
-    :param future_range: range of future (predicted) data required;
-        default 1 day
-    :return: Tuple of compiled dataframe, and the current date
-    """
-    if future_range is None:
-        future_range = pd.to_timedelta(24, unit="h")
-
-    raw_power_data: pd.Series = get_power_data(building_name)
-    start_date: pd.Timestamp = raw_power_data.index[-1]
-    power: pd.Series = group_by_hour(raw_power_data)
-    clouds: pd.Series = group_by_hour(get_weather_data(0))
-    temperature: pd.Series = group_by_hour(get_weather_data(6))
-    occupancy: pd.Series = group_by_hour(
-        get_occupancy_data(
-            building_name,
-            future_range
-        )
-    )
-    data: pd.DataFrame = pd.concat(
-        [power, clouds, temperature, occupancy],
-        axis=1
-    )
-    data.name = building_name
-
-    return data, start_date
-
-
-def group_by_hour(data: pd.Series) -> pd.Series:
-    """
-    Take a time series and produce hourly means.
-    This helps "line up" real world data where observations are mismatched.
-    :param data: time series, whose index is "UTCDateTime"
-    :return: hourly time series
-    """
-    name: str = data.name
-    data: pd.DataFrame = data.groupby(
-        [data.index.date, data.index.hour]
-    ).mean().reset_index()
-    data["level_0"] = pd.to_datetime(data["level_0"])
-    data["UTCDateTime"] = pd.to_timedelta(data["UTCDateTime"], unit="h")
-    data["UTCDateTime"] += data["level_0"]
-    return data.set_index("UTCDateTime")[name]
-
-
-def get_sql(query: str) -> pd.DataFrame:
-    """
-    Fetch SQL data.
-    :param query: SQL query
-    :return: dataframe with index set to UTCDateTime column,
-        formatted as Timestamp
-    """
-    connector: SQLConnector = SQLConnector()
-    data: pd.DataFrame = SQLConnector.exec_sql(connector, query)
-    if "TimeStampUTC" in data:
-        data.rename(columns={"TimeStampUTC": "UTCDateTime"}, inplace=True)
-    data["UTCDateTime"] = pd.to_datetime(data["UTCDateTime"])
-    return data.set_index("UTCDateTime").sort_index()
-
-
-def get_power_data(building_name: str) -> pd.Series:
-    """
-    Retrieve and format power usage data.
-    :param building_name: specific building name;
-        capitalization insensitive
-    :return: time series of power consumption
-    """
-    query: str = "SELECT * FROM "
-    table: str = f"CEVAC_{building_name.upper()}_SPOWER_HIST"
-    data: pd.DataFrame = get_sql(query + table)
-    return data.rename(
-        columns={"Value": "Power [kW]"}
-    )["Power [kW]"]
-
-
-def get_weather_data(mm_id: int) -> pd.Series:
-    """
-    Retrieve and format weather data.
-    :param mm_id: 0 for cloud coverage, 6 for temperature
-    :return: time series of weather data
-    """
-    historical: pd.DataFrame = get_sql(
-        f"SELECT * FROM CEVAC_CAMPUS_WEATHER_HIST WHERE mm_id={mm_id}"
-    )
-    current_date: pd.Timestamp = historical.index[-1]
-
-    forecast: pd.DataFrame = get_sql(
-        f"SELECT * FROM CEVAC_CAMPUS_WEATHER_FORECAST_DAY WHERE mm_id={mm_id}"
-    )
-    forecast = forecast[forecast.index > current_date]
-
-    rename = lambda table: table.rename(
-        columns={
-            "value": "Cloud Coverage [%]" if mm_id == 0 else "Temperature [C]"
-        }
-    )["Cloud Coverage [%]" if mm_id == 0 else "Temperature [C]"]
-
-    historical, forecast = rename(historical), rename(forecast)
-
-    return pd.concat([historical, forecast])
-
-
-def get_occupancy_data(
-        building_name: str,
-        future_range: pd.Timedelta
-) -> pd.Series:
-    """
-    Retrieve and format occupancy data.
-    :param building_name: specific building name;
-        capitalization insensitive
-    :param future_range: range of future (predicted) data required
-    :return: time series of occupancy data
-    """
-    query: str = "SELECT * FROM "
-    table: str = f"CEVAC_{building_name.upper()}_WAP_FLOOR_SUMS_HIST"
-    historical: pd.DataFrame = get_sql(query + table)
-    current_date: pd.Timestamp = historical.index[-1]
-    end_date: pd.Timestamp = current_date + future_range
-    forecast: pd.Series = occupancy_forecast(
-        current_date,
-        end_date
-    )
-    historical = historical.rename(
-        columns={"SUM_clemson_count": "Occupancy"}
-    )["Occupancy"]
-    return pd.concat([historical, forecast])
-
-
-def occupancy_forecast(
-        start_date: pd.Timestamp,
-        end_date: pd.Timestamp
-) -> pd.Series:
-    """
-    Retrieve Helena's occupancy forecast.
-    :param start_date: start of forecast
-    :param end_date: end of forecast
-    :return: time series of occupancy forecast
-    """
-    # TODO: implement Helena's occupancy forecast
-    return pd.Series(
-        name="Occupancy",
-        index=pd.date_range(
-            start=start_date,
-            end=end_date,
-            freq="h",
-            name="UTCDateTime"
-        ),
-        dtype="float64"
-    )
