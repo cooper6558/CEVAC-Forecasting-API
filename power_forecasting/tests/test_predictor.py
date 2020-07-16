6e467defeb1653aa0dcfaeb2b17a065fe45b7c0a
Cooper Sanders <tromboneguy@coopersanders.com>
2020-07-16 17:41:56 -0400

    Added forecast test

--- power_forecasting/tests/test_predictor.py
+++ power_forecasting/tests/test_predictor.py
@@ -3,12 +3,22 @@ Unittest for predictor
 """
 
 import unittest
+from power_forecasting.architectures import LSTM
+from power_forecasting.buildings import Watt
+import pandas as pd
 
 
 class Predictor(unittest.TestCase):
-    from power_forecasting import _predictor
-    def test_something(self):
-        self.assertEqual(True, False)
+    from power_forecasting import _predictor as predictor
+    def setUp(self) -> None:
+        self.model = self.predictor.Predictor(
+            building=Watt,
+            architecture=LSTM
+        )
+    def test_forecast(self):
+        print(self.model.forecast(
+            start_date=pd.Timestamp("2019-11-11")
+        ))
 
 
 if __name__ == '__main__':
